99c0ca9a3a5411da6fe4400dec169f874dc3a120
Deniz <denizderezzed@gmail.com>
2021-04-30 07:30:08 +0200

    layout

--- src/components/PttDisplay.vue
+++ src/components/PttDisplay.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="ptt-display">
-    ptt-display
+    <v-btn
+      tile
+      color="#101818"
+      class="btn-ptt"
+    >
+      <span class="btn-label ptt">PTT</span>
+    </v-btn>
   </div>
 </template>
 
@@ -10,8 +16,18 @@ export default {}
 
 <style lang="scss" scoped>
 .ptt-display {
-  background-color: #101818;
+  background-color: #000000;
   width: 350px;
   height: 250px;
+  .btn-ptt {
+    left: 50%;
+    transform: translateX(-50%);
+    width: 200px;
+    .ptt {
+      font-size: 35px;
+      color: #98f8d0;
+      letter-spacing: 5px;
+    }
+  }
 }
 </style>
