8b34c948475ba49ef9a2513507bc1d8c1b7af9fa
Deniz <denizderezzed@gmail.com>
2021-09-25 09:47:54 +0200

    WIP: radio and searching sounds for hover

--- src/components/PttDisplay.vue
+++ src/components/PttDisplay.vue
@@ -2,30 +2,48 @@
   <div class="ptt-display">
     <v-btn
       tile
-      color="#101818"
+      plain
       class="ptt-btn btn"
     >
       <span class="btn-label">PTT</span>
     </v-btn>
     <div class="display mx-auto">
       <v-card
-        class="my-5"
+        class="my-5 d-flex radio justify-space-between"
         tile
         color="black"
         dark
         height="80%"
       >
-        display
-        <v-sheet
-          color="green"
-          height="20px"
-          width="20px"
-        />
+        <v-btn
+          class="left-arrow arrow align-self-center"
+          plain
+          tile
+        >
+          <v-icon
+            class="icon"
+            style="transform: rotate(180deg)"
+          >
+            mdi-play
+          </v-icon>
+        </v-btn>
+        <div class="radio-display">
+          radio
+        </div>
+        <v-btn
+          class="right-arrow arrow align-self-center"
+          tile
+          plain
+        >
+          <v-icon class="icon">
+            mdi-play
+          </v-icon>
+        </v-btn>
       </v-card>
     </div>
     <v-btn
       tile
-      color="#101818"
+      plain
       class="memory-btn btn"
     >
       <span class="btn-label">MEMORY</span>
@@ -51,6 +69,7 @@ export default {}
     bottom: 6px;
   }
   .btn {
+    background-color: $mgs-dark-btn-green !important;
     -webkit-box-shadow: 0px 0px 0px 8px #000000;
     box-shadow: 0px 0px 0px 8px #000000;
     z-index: 2;
@@ -60,17 +79,35 @@ export default {}
     width: 160px;
     .btn-label {
       font-size: 22px;
-      color: $mgs-light-green;
+      color: $mgs-mid-green;
       letter-spacing: 5px;
     }
+  }
+  .btn:hover .btn-label {
+    color: $mgs-light-green !important;
   }
   .display {
     position: relative;
     border-top: solid $mgs-dark-green 2px;
     border-bottom: solid $mgs-dark-green 2px;
-    width: 95%;
+    width: 100%;
     height: 207px;
     top: -4px;
+    .radio {
+      .arrow {
+        background-color: $mgs-dark-btn-green !important;
+        height: 30px;
+        min-width: 60px;
+        padding: 0;
+        .icon {
+          color: $mgs-mid-green;
+          font-size: 32px;
+        }
+      }
+      .arrow:hover .icon {
+        color: $mgs-light-green;
+      }
+    }
   }
 }
 </style>
